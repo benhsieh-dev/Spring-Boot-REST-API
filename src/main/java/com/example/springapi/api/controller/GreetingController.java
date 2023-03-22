dfa985a2a18458f7f25ae61a16fe0f1d05ef57f2
Ben Hsieh <bqh5026@gmail.com>
2023-03-22 13:57:32 -0500

    Update api end points

--- src/main/java/com/example/springapi/api/controller/GreetingController.java
+++ src/main/java/com/example/springapi/api/controller/GreetingController.java
@@ -1,11 +1,10 @@
-package com.example.springapi.controller;
+package com.example.springapi.api.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
-
     @RequestMapping("/")
     public String getGreeting() {
         return "Nice practice today";
@@ -15,4 +14,5 @@ public class GreetingController {
     public String getMoreFun() {
         return "Let's have fun";
     }
+
 }
